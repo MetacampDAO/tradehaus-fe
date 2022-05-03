04d6c32aab984b9ee1007877a13b23b399f57bce
Jake <jakeyveee@gmail.com>
2022-05-04 01:14:08 +1000

    include connect wallet

--- src/App.tsx
+++ src/App.tsx
@@ -50,7 +50,6 @@ const Context: FC<{ children: ReactNode }> = ({ children }) => {
 };
 
 const Content: FC = () => {
-    // return <WalletMultiButton />;
     return (
         <div>
             <nav className="bg-white border-b border-gray-200 fixed z-30 w-full">
@@ -152,41 +151,8 @@ const Content: FC = () => {
                             </button>
                             <div className="hidden lg:flex items-center">
                                 <span className="text-base font-normal text-gray-500 mr-5">Open source ❤️</span>
-                                <div className="-mb-1">
-                                    <a
-                                        className="github-button"
-                                        href="tradehaus/tradehaus-tailwind-css-dashboard"
-                                        data-color-scheme="no-preference: dark; light: light; dark: light;"
-                                        data-icon="octicon-star"
-                                        data-size="large"
-                                        data-show-count="true"
-                                        aria-label="Star themesberg/tradehaus-tailwind-css-dashboard on GitHub"
-                                    >
-                                        Star
-                                    </a>
-                                </div>
                             </div>
-                            <a
-                                href="/pricing/"
-                                className="hidden sm:inline-flex ml-5 text-white bg-cyan-600 hover:bg-cyan-700 focus:ring-4 focus:ring-cyan-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center items-center mr-3"
-                            >
-                                <svg
-                                    className="svg-inline--fa fa-gem -ml-1 mr-2 h-4 w-4"
-                                    aria-hidden="true"
-                                    focusable="false"
-                                    data-prefix="fas"
-                                    data-icon="gem"
-                                    role="img"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    viewBox="0 0 512 512"
-                                >
-                                    <path
-                                        fill="currentColor"
-                                        d="M378.7 32H133.3L256 182.7L378.7 32zM512 192l-107.4-141.3L289.6 192H512zM107.4 50.67L0 192h222.4L107.4 50.67zM244.3 474.9C247.3 478.2 251.6 480 256 480s8.653-1.828 11.67-5.062L510.6 224H1.365L244.3 474.9z"
-                                    ></path>
-                                </svg>
-                                Upgrade to Pro
-                            </a>
+                            <WalletMultiButton />
                         </div>
                     </div>
                 </div>
