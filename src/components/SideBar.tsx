01c2a8a0d33550b484715be250591516f31a1fed
Jake <jakeyveee@gmail.com>
2022-05-28 13:21:46 +0700

    added documentation

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -100,6 +100,19 @@ const SideBar = () => {
                                 </Link>
                             </li>
                         </ul>
+                        <ul className="pt-1">
+                            <li>
+                                <a
+                                    href="https://jake-17.gitbook.io/tradehaus/about/key-features"
+                                    className="text-base text-gray-900 font-normal rounded-lg hover:bg-gray-100 flex items-center p-2 group "
+                                >
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="w-6 h-6 text-gray-500 flex-shrink-0 group-hover:text-gray-900 transition duration-75" viewBox="0 0 16 16">
+                                        <path d="M8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783z" />
+                                    </svg>
+                                    <span className="ml-3 flex-1 whitespace-nowrap">Documentation</span>
+                                </a>
+                            </li>
+                        </ul>
                     </div>
                 </div>
             </div>
